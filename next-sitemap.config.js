e70e1b281ed48db7604604794527ad9c8246c434
Ananthu <ananthuinkl@gmail.com>
2024-11-17 16:52:58 +0400

    Site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://www.varcloud.online', // Your website URL
+    generateRobotsTxt: true,  // Generate a robots.txt file
+    sitemapSize: 5000,        // Number of URLs per sitemap file (default is 5000)
+    changefreq: 'weekly',     // Default change frequency for URLs
+    priority: 0.7,            // Default priority for all URLs
+  }
+  
